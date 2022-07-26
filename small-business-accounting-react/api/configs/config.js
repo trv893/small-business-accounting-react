35dea46e7208e545a736d7e942ac3bcc8218fbfa
trv893 <trevorhilimire@gmail.com>
2022-07-26 14:38:33 -0400

    qq

--- small-business-accounting-react/api/configs/config.js
+++ small-business-accounting-react/api/configs/config.js
@@ -6,7 +6,7 @@ const myconfig = {
     "database": process.env.DB_NAME,
     "username": process.env.DB_USER,
     "password": process.env.DB_PW,
-    "use_env_variable": true,
+    "use_env_variable": "mysql://fit2v9kc84ljzza8:fbisma7mr5ogfkpk@y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/e2fgoyj4zu4ou961",
     "dialect": "mysql",
 };
 
