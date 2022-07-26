bf4085858fa8c58f1a4fe8fda6fbf928135fc200
trv893 <trevorhilimire@gmail.com>
2022-07-26 13:30:11 -0400

    zz

--- small-business-accounting-react/api/server.js
+++ small-business-accounting-react/api/server.js
@@ -75,6 +75,11 @@ app._router.stack.forEach(function (r) {
 
 app.set('port', process.env.PORT || 3001);
 
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-});
+// var server = app.listen(app.get('port'), function () {
+//     debug('Express server listening on port ' + server.address().port);
+// });
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+  });
