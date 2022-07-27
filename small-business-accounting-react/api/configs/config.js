05232115384149f8a560e1f17f17da695a4b6af0
trv893 <trevorhilimire@gmail.com>
2022-07-27 12:16:02 -0400

    zz

--- small-business-accounting-react/api/configs/config.js
+++ small-business-accounting-react/api/configs/config.js
@@ -2,11 +2,11 @@
 require('dotenv').config()
 
 const myconfig = {
-    "host": process.env.DB_HOST,
-    "database": process.env.DB_NAME,
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PW,
-    "use_env_variable": "mysql://fit2v9kc84ljzza8:fbisma7mr5ogfkpk@y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/e2fgoyj4zu4ou961",
+    "host": "y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "database": "e2fgoyj4zu4ou961",
+    "username": "fit2v9kc84ljzza8",
+    "password": "fbisma7mr5ogfkpk",
+    "use_env_variable": true,
     "dialect": "mysql",
 };
 
